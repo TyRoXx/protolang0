3aa18350400e9f6baa13c20664fa968cbb2eb9bd
TyRoXx <tyroxxxx@gmail.com>
2012-11-13 00:24:31 +0100

    test if ptree serialization works in both directions

--- test/unit_serialization.cpp
+++ test/unit_serialization.cpp
@@ -6,11 +6,76 @@
 #include <boost/test/unit_test.hpp>
 
 
+namespace
+{
+	void check_equal(
+		p0::intermediate::instruction const &first,
+		p0::intermediate::instruction const &second
+		)
+	{
+		BOOST_REQUIRE_EQUAL(first.type(), second.type());
+
+		for (size_t i = 0, c = p0::intermediate::get_instruction_info(
+			first.type()).argument_count; i < c; ++i)
+		{
+			BOOST_CHECK_EQUAL(first.arguments()[i], second.arguments()[i]);
+		}
+	}
+
+	void check_equal(
+		p0::intermediate::function const &first,
+		p0::intermediate::function const &second
+		)
+	{
+		BOOST_CHECK_EQUAL(first.parameters(), second.parameters());
+		BOOST_REQUIRE_EQUAL(first.body().size(), second.body().size());
+
+		for (size_t i = 0, c = first.body().size(); i < c; ++i)
+		{
+			check_equal(
+				first.body()[i],
+				second.body()[i]
+			);
+		}
+	}
+
+	void check_equal(
+		p0::intermediate::unit const &first,
+		p0::intermediate::unit const &second
+		)
+	{
+		BOOST_ASSERT(&first != &second);
+
+		BOOST_CHECK_EQUAL(first.integer_width(), second.integer_width());
+		BOOST_REQUIRE_EQUAL(first.functions().size(), second.functions().size());
+
+		for (size_t f = 0, c = first.functions().size(); f < c; ++f)
+		{
+			check_equal(
+				first.functions()[f],
+				second.functions()[f]
+			);
+		}
+	}
+}
+
 BOOST_AUTO_TEST_CASE(load_save_test)
 {
 	using namespace p0::intermediate;
 
 	unit::function_vector functions;
+	functions.push_back(function(
+		function::instruction_vector(),
+		0));
+
+	functions.push_back(function(
+		function::instruction_vector(),
+		4));
+
+	functions.push_back(function(
+		function::instruction_vector(1, instruction(instruction_type::add, 0, 1)),
+		2));
+
 	unit::string_vector strings;
 	size_t const integer_width = 64;
 
@@ -29,6 +94,8 @@ BOOST_AUTO_TEST_CASE(load_save_test)
 		);
 
 	auto const serialized = sink.str();
+	BOOST_CHECK(!serialized.empty());
+
 	std::istringstream source(serialized);
 	boost::property_tree::ptree loaded_tree;
 	boost::property_tree::read_json(
@@ -38,4 +105,6 @@ BOOST_AUTO_TEST_CASE(load_save_test)
 	auto const loaded_unit = load_unit(
 		loaded_tree
 		);
+
+	check_equal(original_unit, loaded_unit);
 }
