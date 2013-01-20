11f8cd44d01feadb1d920c8f50c6a96654154d8b
TyRoXx <tyroxxxx@gmail.com>
2013-01-20 23:20:25 +0100

    specify entry point

--- p0/main.cpp
+++ p0/main.cpp
@@ -19,9 +19,31 @@ namespace
 		throw std::runtime_error("Not implemented");
 	}
 
-	void execute(p0::intermediate::unit const &unit)
+	struct interpreter
+	{
+		explicit interpreter(p0::intermediate::unit const &program);
+		void call(p0::intermediate::function const &function);
+
+	private:
+
+		p0::intermediate::unit const &m_program;
+	};
+
+	interpreter::interpreter(p0::intermediate::unit const &program)
+		: m_program(program)
+	{
+	}
+
+	void interpreter::call(p0::intermediate::function const &function)
 	{
 		assert(!"not implemented");
+	}
+
+	void execute(p0::intermediate::unit const &program,
+				 p0::intermediate::function const &entry_point)
+	{
+		interpreter interpreter(program);
+		interpreter.call(entry_point);
 	}
 }
 
@@ -30,12 +52,14 @@ int main(int argc, char **argv)
 	namespace po = boost::program_options;
 
 	std::string file_name;
+	std::size_t entry_point_id = 0;
 
 	po::options_description desc("");
 	desc.add_options()
 		("help", "produce help message")
 		("file,f", po::value<std::string>(&file_name), "file name")
 		("compile,c", "")
+		("entry,e", po::value<std::size_t>(&entry_point_id), "entry function id")
 		;
 
 	po::positional_options_description p;
@@ -68,7 +92,20 @@ int main(int argc, char **argv)
 		auto const unit = do_compile_file
 				? p0::compile_unit_from_file(file_name)
 				: load_unit(file_name);
-		execute(unit);
+
+		p0::intermediate::function const *entry_point = 0;
+		if (entry_point_id < unit.functions().size())
+		{
+			entry_point = &unit.functions()[entry_point_id];
+		}
+		else
+		{
+			std::cerr << "Entry function " << entry_point_id
+					  << " does not exist\n";
+			return 1;
+		}
+
+		execute(unit, *entry_point);
 	}
 	catch (std::exception const &ex)
 	{
