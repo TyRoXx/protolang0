fc3700f8260e3d6f7838f9193681051864742e10
TyRoXx <tyroxxxx@gmail.com>
2013-01-30 14:32:32 +0100

    generalize compile and run testing

--- test/compile_and_run.cpp
+++ test/compile_and_run.cpp
@@ -11,18 +11,32 @@ namespace
 		BOOST_CHECK(!"no error expected");
 		return true;
 	}
+
+	template <class CheckResult>
+	void run_valid_source(
+			std::string const &source,
+			std::vector<p0::run::value> const &arguments,
+			CheckResult const &check_result)
+	{
+		p0::source_range const source_range(source.data(), source.data() + source.size());
+		p0::compiler compiler(source_range, expect_no_error);
+		auto const program = compiler.compile();
+
+		p0::run::interpreter interpreter(program);
+		auto const &main_function = program.functions().front();
+		auto const result = interpreter.call(main_function, arguments);
+		check_result(result, program);
+	}
 }
 
 BOOST_AUTO_TEST_CASE(empty_function_compile_test)
 {
 	std::string const source = "{ }";
-	p0::source_range const source_range(source.data(), source.data() + source.size());
-	p0::compiler compiler(source_range, expect_no_error);
-	auto const program = compiler.compile();
-
-	p0::run::interpreter interpreter(program);
-	auto const &main_function = program.functions().front();
 	std::vector<p0::run::value> const arguments;
-	auto const result = interpreter.call(main_function, arguments);
-	BOOST_CHECK(result == p0::run::value(main_function));
+	run_valid_source(source, arguments,
+					 [](p0::run::value const &result, p0::intermediate::unit const &program)
+	{
+		auto const &main_function = program.functions().front();
+		BOOST_CHECK(result == p0::run::value(main_function));
+	});
 }
