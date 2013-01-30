50bc67f752b90d299ad03195bec8812782057c6b
TyRoXx <tyroxxxx@gmail.com>
2013-01-30 14:37:32 +0100

    test return integer, null, string

--- test/compile_and_run.cpp
+++ test/compile_and_run.cpp
@@ -1,6 +1,9 @@
 #include "p0run/interpreter.hpp"
+#include "p0run/string.hpp"
+#include "p0run/table.hpp"
 #include "p0compile/compiler.hpp"
 #include <boost/test/unit_test.hpp>
+using namespace p0::run;
 
 
 namespace
@@ -15,7 +18,7 @@ namespace
 	template <class CheckResult>
 	void run_valid_source(
 			std::string const &source,
-			std::vector<p0::run::value> const &arguments,
+			std::vector<value> const &arguments,
 			CheckResult const &check_result)
 	{
 		p0::source_range const source_range(source.data(), source.data() + source.size());
@@ -37,6 +40,41 @@ BOOST_AUTO_TEST_CASE(empty_function_compile_test)
 					 [](p0::run::value const &result, p0::intermediate::unit const &program)
 	{
 		auto const &main_function = program.functions().front();
-		BOOST_CHECK(result == p0::run::value(main_function));
+		BOOST_CHECK(result == value(main_function));
+	});
+}
+
+BOOST_AUTO_TEST_CASE(return_integer_compile_test)
+{
+	std::string const source = "{ return 123 }";
+	std::vector<value> const arguments;
+	run_valid_source(source, arguments,
+					 [](value const &result, p0::intermediate::unit const &program)
+	{
+		BOOST_CHECK(result == value(static_cast<integer>(123)));
+	});
+}
+
+BOOST_AUTO_TEST_CASE(return_null_compile_test)
+{
+	std::string const source = "{ return null }";
+	std::vector<value> const arguments;
+	run_valid_source(source, arguments,
+					 [](value const &result, p0::intermediate::unit const &program)
+	{
+		BOOST_CHECK(is_null(result));
+	});
+}
+
+BOOST_AUTO_TEST_CASE(return_string_compile_test)
+{
+	std::string const source = "{ return \"hello, world!\" }";
+	std::vector<value> const arguments;
+	run_valid_source(source, arguments,
+					 [](value const &result, p0::intermediate::unit const &program)
+	{
+		BOOST_REQUIRE(result.type == value_type::object);
+		BOOST_REQUIRE(dynamic_cast<string const *>(result.obj));
+		BOOST_CHECK(dynamic_cast<string const &>(*result.obj).content() == "hello, world!");
 	});
 }
