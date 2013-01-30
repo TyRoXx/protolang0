0c562b54a0b2fe54ad591d3226bf3ed7f32ba557
TyRoXx <tyroxxxx@gmail.com>
2013-01-30 14:43:12 +0100

    simple table tests

--- test/compile_and_run.cpp
+++ test/compile_and_run.cpp
@@ -2,6 +2,7 @@
 #include "p0run/string.hpp"
 #include "p0run/table.hpp"
 #include "p0compile/compiler.hpp"
+#include "p0compile/compiler_error.hpp"
 #include <boost/test/unit_test.hpp>
 using namespace p0::run;
 
@@ -10,7 +11,7 @@ namespace
 {
 	bool expect_no_error(p0::compiler_error const &error)
 	{
-		(void)error;
+		std::cerr << error.what() << '\n';
 		BOOST_CHECK(!"no error expected");
 		return true;
 	}
@@ -49,7 +50,7 @@ BOOST_AUTO_TEST_CASE(return_integer_compile_test)
 	std::string const source = "{ return 123 }";
 	std::vector<value> const arguments;
 	run_valid_source(source, arguments,
-					 [](value const &result, p0::intermediate::unit const &program)
+					 [](value const &result, p0::intermediate::unit const & /*program*/)
 	{
 		BOOST_CHECK(result == value(static_cast<integer>(123)));
 	});
@@ -60,7 +61,7 @@ BOOST_AUTO_TEST_CASE(return_null_compile_test)
 	std::string const source = "{ return null }";
 	std::vector<value> const arguments;
 	run_valid_source(source, arguments,
-					 [](value const &result, p0::intermediate::unit const &program)
+					 [](value const &result, p0::intermediate::unit const & /*program*/)
 	{
 		BOOST_CHECK(is_null(result));
 	});
@@ -71,10 +72,38 @@ BOOST_AUTO_TEST_CASE(return_string_compile_test)
 	std::string const source = "{ return \"hello, world!\" }";
 	std::vector<value> const arguments;
 	run_valid_source(source, arguments,
-					 [](value const &result, p0::intermediate::unit const &program)
+					 [](value const &result, p0::intermediate::unit const & /*program*/)
 	{
 		BOOST_REQUIRE(result.type == value_type::object);
 		BOOST_REQUIRE(dynamic_cast<string const *>(result.obj));
 		BOOST_CHECK(dynamic_cast<string const &>(*result.obj).content() == "hello, world!");
+	});
+}
+
+BOOST_AUTO_TEST_CASE(return_empty_table_compile_test)
+{
+	std::string const source = "{ return [] }";
+	std::vector<value> const arguments;
+	run_valid_source(source, arguments,
+					 [](value const &result, p0::intermediate::unit const & /*program*/)
+	{
+		BOOST_REQUIRE(result.type == value_type::object);
+		BOOST_REQUIRE(dynamic_cast<table const *>(result.obj));
+	});
+}
+
+BOOST_AUTO_TEST_CASE(return_trivial_table_compile_test)
+{
+	std::string const source = "{ var t = [] t[123] = 456 return t }";
+	std::vector<value> const arguments;
+	run_valid_source(source, arguments,
+					 [](value const &result, p0::intermediate::unit const & /*program*/)
+	{
+		BOOST_REQUIRE(result.type == value_type::object);
+		BOOST_REQUIRE(dynamic_cast<table const *>(result.obj));
+		auto const element = dynamic_cast<table const &>(*result.obj).get_element(
+					value(static_cast<integer>(123)));
+		BOOST_REQUIRE(element);
+		BOOST_CHECK(*element == value(static_cast<integer>(456)));
 	});
 }
