bd024f296b9ac52f2b4079536f0386e102c396bb
TyRoXx <tyroxxxx@gmail.com>
2013-02-11 21:56:35 +0100

    generalize parser test

--- test/parser.cpp
+++ test/parser.cpp
@@ -13,16 +13,26 @@ namespace
 		BOOST_CHECK(nullptr == "No error expected");
 		return true;
 	}
+
+	template <class ASTHandler>
+	void test_parser(std::string const &source, ASTHandler const &handle_ast)
+	{
+		p0::source_range const source_range(
+					source.data(),
+					source.data() + source.size());
+		p0::scanner scanner(source_range);
+		p0::parser parser(scanner, handle_unexpected_error);
+		auto const ast = parser.parse_unit();
+		BOOST_REQUIRE(ast);
+		handle_ast(*ast);
+	}
 }
 
 BOOST_AUTO_TEST_CASE(parse_empty_file_test)
 {
-	std::string const source = "";
-	p0::source_range const source_range(
-				source.data(),
-				source.data() + source.size());
-	p0::scanner scanner(source_range);
-	p0::parser parser(scanner, handle_unexpected_error);
-	auto const ast = parser.parse_unit();
-	BOOST_REQUIRE(ast);
+	test_parser("", [](p0::function_tree const &ast)
+	{
+		BOOST_CHECK(ast.parameters().empty());
+		//TODO check empty body
+	});
 }
