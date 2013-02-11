cef9321b554b73677c82630a47a6355d90dcded2
TyRoXx <tyroxxxx@gmail.com>
2013-02-11 22:26:24 +0100

    test parse return

--- test/parser.cpp
+++ test/parser.cpp
@@ -1,5 +1,6 @@
 #include "p0compile/parser.hpp"
 #include "p0compile/expression_tree.hpp"
+#include "p0compile/statement_tree.hpp"
 #include "p0compile/scanner.hpp"
 #include "p0compile/compiler_error.hpp"
 #include <boost/test/unit_test.hpp>
@@ -26,6 +27,38 @@ namespace
 		BOOST_REQUIRE(ast);
 		handle_ast(*ast);
 	}
+
+	template <class Base>
+	struct down_caster
+	{
+		down_caster(Base const &base)
+			: m_base(&base)
+		{
+		}
+
+		template <class Down>
+		operator Down const &() const
+		{
+			assert(m_base);
+			return dynamic_cast<Down const &>(*m_base);
+		}
+
+	private:
+
+		Base const *m_base;
+	};
+
+	template <class Handler>
+	void check_expression(p0::expression_tree const &expression, Handler const &handler)
+	{
+		handler(down_caster<p0::expression_tree>(expression));
+	}
+
+	template <class Handler>
+	void check_statement(p0::statement_tree const &statement, Handler const &handler)
+	{
+		handler(down_caster<p0::statement_tree>(statement));
+	}
 }
 
 BOOST_AUTO_TEST_CASE(parse_empty_file_test)
@@ -33,6 +66,27 @@ BOOST_AUTO_TEST_CASE(parse_empty_file_test)
 	test_parser("", [](p0::function_tree const &ast)
 	{
 		BOOST_CHECK(ast.parameters().empty());
-		//TODO check empty body
+		check_statement(ast.body(), [](p0::block_tree const &block)
+		{
+			BOOST_CHECK(block.body().empty());
+		});
+	});
+}
+
+BOOST_AUTO_TEST_CASE(parse_return_test)
+{
+	test_parser("return null", [](p0::function_tree const &ast)
+	{
+		BOOST_CHECK(ast.parameters().empty());
+		check_statement(ast.body(), [](p0::block_tree const &block)
+		{
+			BOOST_REQUIRE(block.body().size() == 1);
+			check_statement(*block.body()[0], [](p0::return_tree const &return_)
+			{
+				check_expression(return_.value(), [](p0::null_expression_tree const &)
+				{
+				});
+			});
+		});
 	});
 }
