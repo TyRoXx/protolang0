2349dcea6530797180e56e2be33ef7fb9fbf814e
TyRoXx <tyroxxxx@gmail.com>
2013-03-02 17:06:12 +0100

    compatibility macros for Clang

--- p0common/final.hpp
+++ p0common/final.hpp
@@ -5,14 +5,26 @@
 
 #ifdef _MSC_VER
 
+//currently only VC++ 2010 is supported, and it knows override but not final
 #	define PROTOLANG0_FINAL_METHOD override
+#	define PROTOLANG0_FINAL_CLASS
+
+#elif defined (__clang__)
+
+//override and final supported since Clang 3.0 (http://clang.llvm.org/cxx_status.html)
+#if (__clang_major__ >= 3)
+#	define PROTOLANG0_FINAL_METHOD final
+#	define PROTOLANG0_FINAL_CLASS final
+#endif
 
 #elif defined (__GNUC__)
 
 #	if (__GNUC__ >= 4) && ((__GNUC__ > 4) || (__GNUC_MINOR__ >= 7))
 #		define PROTOLANG0_FINAL_METHOD final
+#		define PROTOLANG0_FINAL_CLASS final
 #	else
 #		define PROTOLANG0_FINAL_METHOD
+#		define PROTOLANG0_FINAL_CLASS
 #	endif
 
 #else
