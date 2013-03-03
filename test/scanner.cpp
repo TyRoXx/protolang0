86eb016dc363e1204f2ce97047179fe477a7779d
TyRoXx <tyroxxxx@gmail.com>
2013-03-03 21:38:23 +0100

    test file for lexical error detection

--- test/scanner.cpp
+++ test/scanner.cpp
@@ -149,60 +149,6 @@ BOOST_AUTO_TEST_CASE(identifier_test)
 
 namespace
 {
-	bool check_expected_exception(
-		std::string const &code,
-		std::size_t position
-		)
-	{
-		p0::scanner scanner(p0::source_range(code.data(), code.data() + code.size()));
-
-		try
-		{
-			while (scanner.next_token().type != p0::token_type::end_of_file)
-			{
-			}
-		}
-		catch (p0::compiler_error const &e)
-		{
-			auto const error_pos = static_cast<size_t>(
-				std::distance(code.data(), e.position().begin()));
-			return (position == error_pos);
-		}
-
-		return false;
-	}
-}
-
-BOOST_AUTO_TEST_CASE(general_error_test)
-{
-	BOOST_CHECK(check_expected_exception("abc?", 3));
-	BOOST_CHECK(check_expected_exception("?", 0));
-	BOOST_CHECK(check_expected_exception("\\", 0));
-	BOOST_CHECK(check_expected_exception("123e", 3));
-}
-
-BOOST_AUTO_TEST_CASE(string_error_test)
-{
-	//these characters may not occur unescaped in a string
-	BOOST_CHECK(check_expected_exception("\"\xff\"", 1));
-	BOOST_CHECK(check_expected_exception("\"\x7f\"", 1));
-	BOOST_CHECK(check_expected_exception("\"\x1f\"", 1));
-	BOOST_CHECK(check_expected_exception("\"\x00\"", 1));
-	BOOST_CHECK(check_expected_exception("\"\x05\"", 1));
-	BOOST_CHECK(check_expected_exception("\"\x0a\"", 1));
-
-	//missing closing quote
-	BOOST_CHECK(check_expected_exception("\"hallo", 6));
-
-	//missing escape character(s) after backslash
-	BOOST_CHECK(check_expected_exception("\"hallo\\", 7));
-
-	//the second quote is escaped so it is expected not to close the string
-	BOOST_CHECK(check_expected_exception("\"hallo\\\"", 8));
-}
-
-namespace
-{
 	bool is_integer(
 		std::string const &value,
 		p0::token const &token
