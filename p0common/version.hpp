890e1dea9c6c1a952b10e3a7bfd533babc8ad85a
TyRoXx <tyroxxxx@gmail.com>
2013-05-21 20:49:22 +0200

    add header for library version number

---
+++ p0common/version.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef P0COMMON_VERSION_HPP
+#define P0COMMON_VERSION_HPP
+
+
+#define PROTOLANG0_VERSION 100
+
+
+namespace p0
+{
+	static unsigned const version = PROTOLANG0_VERSION;
+}
+
+
+#endif
