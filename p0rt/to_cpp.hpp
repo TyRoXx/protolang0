ea37ff1e5e90e3ccf72f860c41fa3b97703b124f
TyRoXx <tyroxxxx@gmail.com>
2013-07-15 23:19:38 +0200

    to_cpp supports {std,boost}::unordered_map

--- p0rt/to_cpp.hpp
+++ p0rt/to_cpp.hpp
@@ -4,8 +4,10 @@
 
 
 #include "p0run/string.hpp"
-#include <map>
 #include <stdexcept>
+#include <map>
+#include <unordered_map>
+#include <boost/unordered_map.hpp>
 
 
 namespace p0
@@ -32,44 +34,67 @@ namespace p0
 			to = run::expect_string(from);
 		}
 
+		namespace detail
+		{
+			template <class Key, class Value, class Map>
+			void to_cpp_map(Map &to, run::value from)
+			{
+				if (from.type != run::value_type::object)
+				{
+					throw std::runtime_error("Cannot construct std::map from a non-object");
+				}
+
+				struct map_builder : run::object_element_callback
+				{
+					explicit map_builder(Map &to)
+					    : to(to)
+					{
+					}
+
+					virtual bool handle_element(run::value key,
+					                            run::value value) PROTOLANG0_OVERRIDE
+					{
+						using p0::rt::to_cpp;
+
+						Key cpp_key;
+						to_cpp(cpp_key, key);
+
+						Value cpp_value;
+						to_cpp(cpp_value, value);
+
+						to.insert(std::make_pair(std::move(cpp_key), std::move(cpp_value)));
+						return true;
+					}
+
+				private:
+
+					Map &to;
+				};
+
+				map_builder callback((to));
+				from.obj->enumerate_elements(callback);
+			}
+		}
+
 		template <class Key, class Value, class Compare, class Alloc>
 		void to_cpp(std::map<Key, Value, Compare, Alloc> &to,
 		            run::value from)
 		{
-			if (from.type != run::value_type::object)
-			{
-				throw std::runtime_error("Cannot construct std::map from a non-object");
-			}
+			return detail::to_cpp_map<Key, Value>(to, from);
+		}
 
-			struct map_builder : run::object_element_callback
-			{
-				explicit map_builder(std::map<Key, Value, Compare, Alloc> &to)
-				    : to(to)
-				{
-				}
+		template <class Key, class Value, class Hash, class Pred, class Alloc>
+		void to_cpp(std::unordered_map<Key, Value, Hash, Pred, Alloc> &to,
+		            run::value from)
+		{
+			return detail::to_cpp_map<Key, Value>(to, from);
+		}
 
-				virtual bool handle_element(run::value key,
-				                            run::value value) PROTOLANG0_OVERRIDE
-				{
-					using p0::rt::to_cpp;
-
-					Key cpp_key;
-					to_cpp(cpp_key, key);
-
-					Value cpp_value;
-					to_cpp(cpp_value, value);
-
-					to.insert(std::make_pair(std::move(cpp_key), std::move(cpp_value)));
-					return true;
-				}
-
-			private:
-
-				std::map<Key, Value, Compare, Alloc> &to;
-			};
-
-			map_builder callback((to));
-			from.obj->enumerate_elements(callback);
+		template <class Key, class Value, class Hash, class Pred, class Alloc>
+		void to_cpp(boost::unordered_map<Key, Value, Hash, Pred, Alloc> &to,
+		            run::value from)
+		{
+			return detail::to_cpp_map<Key, Value>(to, from);
 		}
 	}
 }
--- test/to_cpp.cpp
+++ test/to_cpp.cpp
@@ -4,21 +4,32 @@
 #include <boost/test/unit_test.hpp>
 using namespace p0;
 
+namespace
+{
+	template <class Map>
+	void test_map()
+	{
+		run::default_garbage_collector gc;
+		run::table table;
+		rt::inserter(table, gc)
+		        .insert("a", 0)
+		        .insert("b", 1)
+		        .insert("c", 2)
+		        ;
+
+		Map map;
+		rt::to_cpp(map, run::value(table));
+
+		BOOST_REQUIRE_EQUAL(map.size(), 3);
+		BOOST_CHECK_EQUAL(map["a"], 0);
+		BOOST_CHECK_EQUAL(map["b"], 1);
+		BOOST_CHECK_EQUAL(map["c"], 2);
+	}
+}
+
 BOOST_AUTO_TEST_CASE(to_cpp_map_test)
 {
-	run::default_garbage_collector gc;
-	run::table table;
-	rt::inserter(table, gc)
-	        .insert("a", 0)
-	        .insert("b", 1)
-	        .insert("c", 2)
-	        ;
-
-	std::map<std::string, run::integer> map;
-	rt::to_cpp(map, run::value(table));
-
-	BOOST_REQUIRE_EQUAL(map.size(), 3);
-	BOOST_CHECK_EQUAL(map["a"], 0);
-	BOOST_CHECK_EQUAL(map["b"], 1);
-	BOOST_CHECK_EQUAL(map["c"], 2);
+	test_map<std::map<std::string, run::integer>>();
+	test_map<std::unordered_map<std::string, run::integer>>();
+	test_map<boost::unordered_map<std::string, run::integer>>();
 }
