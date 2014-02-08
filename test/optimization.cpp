d6f4a83e70ff4336ae34a5e1930d81fda0303913
TyRoXx <tyroxxxx@gmail.com>
2014-02-08 19:58:35 +0100

    test that fold_constants does not try to follow jumps

--- test/optimization.cpp
+++ test/optimization.cpp
@@ -90,3 +90,24 @@ BOOST_AUTO_TEST_CASE(fold_constants_tree)
 	}
 	BOOST_CHECK(expected_code == cleaned_code);
 }
+
+BOOST_AUTO_TEST_CASE(fold_constants_do_not_follow_jump)
+{
+	p0::intermediate::function::instruction_vector original_code;
+	{
+		p0::intermediate::emitter emitter(original_code);
+		/*0*/ emitter.set_constant(0, 2);
+		/*1*/ emitter.set_constant(1, 3); //obsolete
+		/*2*/ emitter.jump(6);
+		/*3*/ emitter.set_constant(1, 4); //overwrites the write by 1
+		/*5*/ emitter.add(0, 1);
+		/*4*/ emitter.jump(7);
+		/*6*/ emitter.jump(3);
+		/*7 return*/
+	}
+	p0::intermediate::function original_function(original_code, 0, 0);
+	p0::intermediate::function::instruction_vector const folded_code = p0::fold_constants(original_function);
+
+	//We expect fold_constants not to follow jump paths. That is the job of earlier passes.
+	BOOST_CHECK(original_code == folded_code);
+}
