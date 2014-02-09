51f79f647de4d93e6fb7961b310fead511e87187
TyRoXx <tyroxxxx@gmail.com>
2014-02-09 19:39:46 +0100

    add an inlining test with a non-empty callee (fails)

--- test/inlining.cpp
+++ test/inlining.cpp
@@ -21,3 +21,27 @@ BOOST_AUTO_TEST_CASE(inlining_empty_callee)
 	}
 	BOOST_CHECK(expected_code == optimized);
 }
+
+BOOST_AUTO_TEST_CASE(inlining_trivially_returning_callee)
+{
+	p0::intermediate::function::instruction_vector callee_code;
+	{
+		p0::intermediate::emitter emitter(callee_code);
+		emitter.set_constant(0, 12);
+	}
+	p0::intermediate::function callee(callee_code, 0, 0);
+	std::vector<p0::intermediate::function> environment{callee};
+	p0::intermediate::function::instruction_vector original_code;
+	{
+		p0::intermediate::emitter emitter(original_code);
+		emitter.set_function(0, 0);
+		emitter.call(0, 0);
+	}
+	p0::intermediate::function::instruction_vector const optimized = p0::inlining(original_code, environment);
+	p0::intermediate::function::instruction_vector expected_code;
+	{
+		p0::intermediate::emitter emitter(expected_code);
+		emitter.set_constant(0, 12);
+	}
+	BOOST_CHECK(expected_code == optimized);
+}
