b16148e1cb40a07ed38a2c25b53af641b18f7e8c
TyRoXx <tyroxxxx@gmail.com>
2014-02-10 21:03:25 +0100

    implement dissect_function

--- p0optimize/analyzation.cpp
+++ p0optimize/analyzation.cpp
@@ -1,6 +1,8 @@
 #include "analyzation.hpp"
 #include <boost/foreach.hpp>
 #include <cassert>
+#include <unordered_map>
+#include <set>
 
 namespace p0
 {
@@ -25,8 +27,83 @@ namespace p0
 		return destinations;
 	}
 
+	namespace
+	{
+		struct section_end
+		{
+			p0::intermediate::instruction const *instruction_after;
+			std::vector<std::size_t> destinations;
+		};
+
+		template <class InstructionPtrPredicate>
+		section_end find_section_end(const_instruction_range code, InstructionPtrPredicate const &is_jump_destination, std::size_t code_offset)
+		{
+			std::set<std::size_t> destinations;
+			auto i = code.begin();
+			for (;;)
+			{
+				if (i == code.end())
+				{
+					destinations.insert(code_offset + code.size());
+					break;
+				}
+
+				if ((i != code.begin()) && is_jump_destination(i))
+				{
+					break;
+				}
+
+				if (p0::intermediate::is_any_jump(i->type()))
+				{
+					auto const destination = i->arguments()[0];
+					destinations.insert(destination);
+					if (i->type() != p0::intermediate::instruction_type::jump)
+					{
+						//jump_if, jump_if_not
+						destinations.insert(code_offset + std::distance(code.begin(), i) + 1);
+					}
+					++i;
+					break;
+				}
+
+				++i;
+			}
+			return section_end{i, std::vector<std::size_t>(begin(destinations), end(destinations))};
+		}
+	}
+
 	function_section_graph dissect_function(p0::intermediate::function::instruction_vector const &code)
 	{
-		return function_section_graph{};
+		auto const jump_destinations = find_all_jump_destinations(code);
+		std::vector<linear_section> sections;
+		const_instruction_range rest{code.data(), code.data() + code.size()};
+		std::unordered_map<std::size_t, section_id> instruction_to_section;
+		while (!rest.empty())
+		{
+			auto const is_jump_destination = [&jump_destinations, &code](p0::intermediate::instruction const *i)
+			{
+				return (jump_destinations.find(std::distance(code.data(), i)) != jump_destinations.end());
+			};
+			auto const code_offset = std::distance(code.data(), rest.begin());
+			auto const section_end = find_section_end(
+						rest,
+						is_jump_destination,
+						code_offset);
+			for (auto i = code_offset, e = code_offset + std::distance(rest.begin(), section_end.instruction_after); i != e; ++i)
+			{
+				instruction_to_section.insert(std::make_pair(i, sections.size()));
+			}
+			sections.emplace_back(linear_section{{rest.begin(), section_end.instruction_after}, std::move(section_end.destinations)});
+			rest = {section_end.instruction_after, rest.end()};
+		}
+		instruction_to_section.insert(std::make_pair(code.size(), sections.size()));
+		BOOST_FOREACH (auto &section, sections)
+		{
+			std::for_each(begin(section.destinations), end(section.destinations), [&instruction_to_section](std::size_t &destination)
+			{
+				destination = instruction_to_section[destination];
+			});
+		}
+		return function_section_graph{std::move(sections)};
 	}
 }
--- test/analyzation.cpp
+++ test/analyzation.cpp
@@ -6,14 +6,10 @@ BOOST_AUTO_TEST_CASE(dissect_function_empty)
 {
 	p0::intermediate::function::instruction_vector const code;
 	p0::function_section_graph const g = p0::dissect_function(code);
-	BOOST_REQUIRE(g.sections.size() == 1);
-	p0::linear_section const &section = g.sections.front();
-	BOOST_CHECK(section.code.begin() == code.data());
-	BOOST_CHECK(section.code.end() == code.data());
-	BOOST_CHECK(section.destinations.empty());
+	BOOST_CHECK(g.sections.empty());
 }
 
-BOOST_AUTO_TEST_CASE(dissect_function_if)
+BOOST_AUTO_TEST_CASE(dissect_function_jump_if)
 {
 	p0::intermediate::function::instruction_vector code;
 	{
@@ -31,12 +27,40 @@ BOOST_AUTO_TEST_CASE(dissect_function_if)
 		p0::linear_section const &section = g.sections[0];
 		BOOST_CHECK(section.code.begin() == code.data());
 		BOOST_CHECK(section.code.end() == code.data() + 2);
-		BOOST_CHECK(section.destinations == std::vector<p0::section_id>{1});
+		BOOST_CHECK((section.destinations == std::vector<p0::section_id>{1, 2}));
 	}
 	{
 		p0::linear_section const &section = g.sections[1];
 		BOOST_CHECK(section.code.begin() == code.data() + 2);
 		BOOST_CHECK(section.code.end() == code.data() + 3);
-		BOOST_CHECK(section.destinations == std::vector<p0::section_id>{});
+		BOOST_CHECK(section.destinations == std::vector<p0::section_id>{2});
+	}
+}
+
+BOOST_AUTO_TEST_CASE(dissect_function_jump)
+{
+	p0::intermediate::function::instruction_vector code;
+	{
+		// return (arg0 ? 2 : 3)
+		p0::intermediate::emitter emitter(code);
+		//section 0
+		emitter.set_constant(0, 2);
+		emitter.jump(3);
+		//section 1
+		emitter.set_constant(0, 3);
+	}
+	p0::function_section_graph const g = p0::dissect_function(code);
+	BOOST_REQUIRE(g.sections.size() == 2);
+	{
+		p0::linear_section const &section = g.sections[0];
+		BOOST_CHECK(section.code.begin() == code.data());
+		BOOST_CHECK(section.code.end() == code.data() + 2);
+		BOOST_CHECK((section.destinations == std::vector<p0::section_id>{2}));
+	}
+	{
+		p0::linear_section const &section = g.sections[1];
+		BOOST_CHECK(section.code.begin() == code.data() + 2);
+		BOOST_CHECK(section.code.end() == code.data() + 3);
+		BOOST_CHECK(section.destinations == std::vector<p0::section_id>{2});
 	}
 }
